f3189b053be8c4d2dcf64d7b0dd0de53c829574c
Meyazhagan <meyazhagan.ofcl@gmail.com>
2020-07-15 11:50:54 +0530

    init method

---
+++ Chip-8 Emulator/Chip.java
@@ -0,0 +1,31 @@
+public class Chip{
+  private char[] memory; //memory 4k
+  private char[] V; //register
+  private char I; //address pointer
+  private char pc;
+
+  private char[] stack;
+  private int stackPointer;
+
+  private int display_timer;
+  private int sound_timer;
+
+  private byte[] keys;
+  private byte[] display;
+
+  public void init(){
+    memory = new char[4096];
+    V = new char[16];
+    I = 0x0;
+    pc = 0x200;
+
+    stack = new char[16];
+    stackPointer = 0;
+
+    display_timer = 0;
+    sound_timer = 0;
+
+    keys = new byte[16];
+    display = new byte[64* 32];
+  }
+}
