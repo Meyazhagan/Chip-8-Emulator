cc55a9fd96a7fed1b3a501815193a889c9b4ec49
Meyazhagan <meyazhagan.ofcl@gmail.com>
2020-07-17 18:10:02 +0530

    audio added

---
+++ chip8/Audio.java
@@ -0,0 +1,25 @@
+package chip8;
+
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class Audio {
+  static int i =0;
+  public static void playSound(String file){
+    try{
+    Clip clip = AudioSystem.getClip();
+    AudioInputStream audio = AudioSystem.getAudioInputStream(new File(file));
+    clip.open(audio);
+    clip.start();
+    
+    System.out.println("Beep " + i++);
+    }
+    catch(Exception e){
+      System.err.println("error in audio");
+    }
+    
+  }
+}
